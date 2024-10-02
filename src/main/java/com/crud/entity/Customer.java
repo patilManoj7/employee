a594871c5f551d1ac8e678ed53f8b68c02f674ff
patilManoj7 <manojpatil72002@gmail.com>
2024-10-02 12:42:29 +0530

    changes in customer entity

--- src/main/java/com/crud/entity/Customer.java
+++ src/main/java/com/crud/entity/Customer.java
@@ -5,13 +5,15 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="customer")
+@Table(name = "customer")
 public class Customer {
     @Id
-            @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
-
+    @Column(nullable = false)
     String name;
+    @Column(unique = true)
     String email;
+    @Column(length = 10)
     String phone;
 }
